03ae1ac5eb4c810c480f4348e07ef296af1079e7
SavSamoylov <ssamoylov@remedyhealthmedia.com>
2021-03-25 19:27:39 -0400

    formatting md

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 
+const createToC = (cTypes = []) => {
+  let str = ''
+  cTypes.forEach(c => {
+    str+=`* [${c}](#${c.replace(/\s+/g, '-').toLowerCase()})\n`
+  })
+  return str
+}
+
 async function queryContentful() {
   try {
     const spaceId = core.getInput('space_id');
@@ -23,19 +31,28 @@ async function queryContentful() {
       const strippedObj = {
         name,
         description,
-        fields: f.map(({name, id, type, required}) => ([name, id, type === 'Symbol' ? 'String' : type, required]))
+        fields: f.map(({name, id, type, required}) => ([name, id, type === 'Symbol' ? 'Symbol (String)' : type, required]))
       }
-      strippedObj.fields.unshift(['name', 'id', 'type', 'required'])
+      strippedObj.fields.unshift(['Name', 'ID', 'Type', 'Required'])
       arr.push(strippedObj)
       return arr
     }, [])
     
     console.log(JSON.stringify(formattedRes, 0, 2))
+    
+    const buildMarkdown = `
+Table of Contents
+${createToC(formattedRes.map(item => item.name))}
+`
+    
+    console.log(buildMarkdown)
+    
+    
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
