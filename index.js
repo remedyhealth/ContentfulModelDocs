34dc3f1a2befc919c30e329269f52cf77936e354
SavSamoylov <ssamoylov@remedyhealthmedia.com>
2021-03-25 19:01:03 -0400

    format res

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ async function queryContentful() {
     const envId = core.getInput('environment_id');
     const accessToken = core.getInput('access_token');
     core.setSecret(spaceId)
-    core.setSecret(envId)
     core.setSecret(accessToken)
-    
     
     const response = await fetch(`https://cdn.contentful.com/spaces/${spaceId}/environments/${envId}/content_types?access_token=${accessToken}`)
       .then(res => res.json())
       .catch(err => {
         err.contentfulId = response && response.headers && response.headers.get('x-contentful-request-id')
-        logger.error('Error during rest request', err)
         core.setFailed(`Failed to query Contentful ${err}`)
-        throw err
       })
     
+    const formattedRes = response.items.reduce((prevArr, cType) => {
+      const arr = prevArr || []
+      const { name, description, fields: f } = cType
+      const strippedObj = {
+        name,
+        description,
+        fields: f.map(({name, type, required}) => ([name, type === 'Symbol' ? 'String' : type, required]))
+      }
+      return arr.push(strippedObj)
+    }, [])
     
-    console.log(JSON.stringify(response, 0, 2))
+    console.log(JSON.stringify(formattedRes, 0, 2))
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
