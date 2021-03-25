c47b6fc81d467d3ffba87533a9c3a3b73b8cbc7c
SavSamoylov <ssamoylov@remedyhealthmedia.com>
2021-03-25 16:19:08 -0400

    attempting to fetch

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
 try {
-  const secretId = core.getInput('space_id');
-  core.setSecret(secretId)
+  const spaceId = core.getInput('space_id');
   const envId = core.getInput('environment_id');
   const accessToken = core.getInput('access_token');
-  console.log(secretId)
+  core.setSecret(spaceId)
+  core.setSecret(envId)
+  core.setSecret(accessToken)
+  
+  
+  const response = await fetch(`https://cdn.contentful.com/spaces/${spaceId}/environments/${envId}/content_types?access_token=${access_token}`)
+    .then(res => res.json())
+    .catch(err => {
+      err.contentfulId = response && response.headers && response.headers.get('x-contentful-request-id')
+      logger.error('Error during rest request', err)
+      core.setFailed(`Failed to query Contentful ${err}`)
+      throw err
+    })
+  
+  
+  console.log(JSON.stringify(response, 0, 2))
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
