e8fc0d5fb11506607e31a85946b62eaf034d4e43
SavSamoylov <ssamoylov@remedyhealthmedia.com>
2021-03-25 16:06:17 -0400

    custom inputs

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const secretId = core.getInput('who-to-greet');
-  const envId = core.getInput('who-to-greet');
-  const accessToken = core.getInput('who-to-greet');
+  const secretId = core.getInput('space_id');
+  const envId = core.getInput('environment_id');
+  const accessToken = core.getInput('access_token');
   console.log(core.setSecret(secretId))
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
